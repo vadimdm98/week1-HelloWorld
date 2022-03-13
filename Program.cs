f01441702a0f3d79f8bc45fc160a984fec3995af
Vadim Dmitrijev <vadim.dmitrijev1998@gmail.com>
2022-03-13 21:12:12 +0200

    add my first C# project

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is you first name?");
+            string userFirstName =Console.ReadLine();
+            Console.WriteLine("What is your last name");
+            string userLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + userFirstName + " " + userLastName + "!");
+
+
+
+
+
+
+        }
+    }
+}
